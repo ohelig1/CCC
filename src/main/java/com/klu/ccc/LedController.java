c3fade3b70bfa5abe9bd19e8a1122973c8842c27
ohelig <amor.hsinay@gmail.com>
2018-05-09 01:06:13 +0545

    Pi4j version 1.2

--- src/main/java/com/klu/ccc/LedController.java
+++ src/main/java/com/klu/ccc/LedController.java
@@ -2,7 +2,6 @@ package com.klu.ccc;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.klu.error.CCCError;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
@@ -11,6 +10,7 @@ import com.pi4j.io.gpio.RaspiPin;
 
 @RestController
 public class LedController {
+	private static final GpioController gpio = GpioFactory.getInstance();
 	private static GpioPinDigitalOutput pin;
 
 	@RequestMapping("/")
@@ -33,68 +33,21 @@ public class LedController {
 	private String checkState() {
 		return (getPin().isHigh() ? "Light is ON" : "Light is OFF");
 	}
-
-	@RequestMapping("/on26")
+	@RequestMapping("/on")
 	public String on() {
-		GpioController gpio = GpioFactory.getInstance();
-		pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26, "MyLED", PinState.LOW);
-		pin.high();
+		getPin().high();
 		return checkState();
 	}
 
-	@RequestMapping("/off26")
+	@RequestMapping("/off")
 	public String off() {
-		GpioController gpio = GpioFactory.getInstance();
-		pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26, "MyLED", PinState.LOW);
 		getPin().low();
 		return checkState();
 	}
-
-	@RequestMapping("/off25")
-	public String off4() {
-		GpioController gpio = GpioFactory.getInstance();
-		pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "MyLED", PinState.LOW);
-		pin.low();
-		return checkState();
-	}
-
-	@RequestMapping("/on25")
-	public String on4() throws CCCError {
-		GpioController gpio = GpioFactory.getInstance();
-		pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "MyLED", PinState.LOW);
-		pin.high();
-		return checkState();
-	}
-
-	@RequestMapping("/state25")
-	public String status() {
-		return getPin25().getState().toString();
-	}
-
-	@RequestMapping("/blink")
-	public String blink() throws CCCError {
-		getPin().blink(200L, 5000L);
-		return "Light is blinking...";
-	}
-
-	@RequestMapping("/pulse")
-	public String pulse() throws CCCError {
-		getPin().pulse(5000L);
-		return "Light is pulsing...";
-	}
-
+	
 	private GpioPinDigitalOutput getPin() {
 		if (pin == null) {
-			GpioController gpio = GpioFactory.getInstance();
 			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
-		}
-		return pin;
-	}
-
-	private GpioPinDigitalOutput getPin25() {
-		if (pin == null) {
-			GpioController gpio = GpioFactory.getInstance();
-			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "MyLED", PinState.LOW);
 		}
 		return pin;
 	}
