3fe3dfd9663bdb3280a53fcfb0ae5c5e05a349a3
ohelig <amor.hsinay@gmail.com>
2018-10-02 00:56:53 +0530

    Adding Red Yellow and Green

--- src/main/java/com/klu/ccc/LedController.java
+++ src/main/java/com/klu/ccc/LedController.java
@@ -1,7 +1,9 @@
 package com.klu.ccc;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
@@ -18,49 +20,57 @@ public class LedController {
 
 		return "Hello World!!!";
 	}
-
 	@RequestMapping("/home")
 	public String home() {
 		return "Hello Home!!!";
 	}
 
-	@RequestMapping("/toggle")
-	public String light() {
-		getPin().toggle();
+	@RequestMapping("/{colour}/toggle")
+	public String light(@PathVariable("colour") String colour) {
+		getPin(colour).toggle();
 		return "OK";
 	}
 
-	private String checkState() {
-		return (getPin().isHigh() ? "Light is ON" : "Light is OFF");
+	private String checkState(String colour) {
+		return (getPin(colour).isHigh() ? "Light is ON" : "Light is OFF");
 	}
-	@RequestMapping("/on")
-	public String on() {
-		getPin().high();
-		return checkState();
+	@RequestMapping("/{colour}/on")
+	public String on(@PathVariable("colour") String colour) {
+		getPin(colour).high();
+		return checkState(colour);
 	}
 
-	@RequestMapping("/off")
-	public String off() {
-		getPin().low();
-		return checkState();
+	@RequestMapping("/{colour}/off")
+	public String off(@PathVariable("colour") String colour) {
+		getPin(colour).low();
+		return checkState(colour);
 	}
 	
-	@RequestMapping("/blink")
-	public String blink() {
-		getPin().blink(200L,5000L);
+	@RequestMapping("/{colour}/blink")
+	public String blink(@PathVariable("colour") String colour) {
+		getPin(colour).blink(200L,5000L);
 		return "Light is blinking...";
 	}
 	
 	@RequestMapping("/pulse")
-	public String pulse() {
-		getPin().pulse(5000L);
+	public String pulse(@PathVariable("colour") String colour) {
+		getPin(colour).pulse(5000L);
 		return "Light is pulsing...";
 	}
 	
-	private GpioPinDigitalOutput getPin() {
+	private GpioPinDigitalOutput getPin(String colour) {
 		if (pin == null) {
-			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+			if(colour.equals("red")) {
+				pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+			}
+			if(colour.equals("yellow")) {
+				pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.LOW);
+			}
+			if(colour.equals("green")) {
+				pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "MyLED", PinState.LOW);
+			}
 		}
 		return pin;
 	}
 }
+
