e09e8df664bc142aa1832041977f6821b7600ce1
ohelig <amor.hsinay@gmail.com>
2018-10-02 02:43:58 +0530

    BugFix for GPIO pin already exists

--- src/main/java/com/klu/ccc/LedController.java
+++ src/main/java/com/klu/ccc/LedController.java
@@ -3,6 +3,7 @@ package com.klu.ccc;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.klu.utility.CommonUtils;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
@@ -18,51 +19,57 @@ public class LedController {
 		return "Hello World!!!";
 	}
 
-	@RequestMapping("/home")
-	public String home() {
-		return "Hello Home!!!";
-	}
-
 	@RequestMapping("/{colour}/toggle")
 	public String light(@PathVariable("colour") String colour) {
-		getPin(colour).toggle();
-		return "OK";
+		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), "MyLED", PinState.LOW);
+		gpioPinDigitalOutput.toggle();
+		gpio.shutdown(); 
+		gpio.unprovisionPin(gpioPinDigitalOutput);
+		return "Toggle Successful";
 	}
 
 	@RequestMapping("/{colour}/status")
 	private String getPinStatus(@PathVariable("colour") String colour) {
-		return (getPin(colour).isHigh() ? "Light is ON" : "Light is OFF");
+		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), "MyLED", PinState.LOW);
+		String status = gpioPinDigitalOutput.isHigh() ? "Light is ON" : "Light is OFF";
+		gpio.shutdown(); 
+		gpio.unprovisionPin(gpioPinDigitalOutput);
+		return status;
 	}
 
 	@RequestMapping("/{colour}/on")
 	public String on(@PathVariable("colour") String colour) {
-		getPin(colour).high();
-		return checkState(colour);
+		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), "MyLED", PinState.LOW);
+		gpioPinDigitalOutput.high();
+		gpio.shutdown(); 
+		gpio.unprovisionPin(gpioPinDigitalOutput);
+		return "Light is ON!!!";
 	}
 
 	@RequestMapping("/{colour}/off")
 	public String off(@PathVariable("colour") String colour) {
-		getPin(colour).low();
-		return checkState(colour);
+		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), "MyLED", PinState.LOW);
+		gpioPinDigitalOutput.low();
+		gpio.shutdown(); 
+		gpio.unprovisionPin(gpioPinDigitalOutput);
+		return "Light is OFF!!!";
 	}
 
 	@RequestMapping("/{colour}/blink")
 	public String blink(@PathVariable("colour") String colour) {
-		getPin(colour).blink(200L, 5000L);
+		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), "MyLED", PinState.LOW);
+		gpioPinDigitalOutput.blink(200L, 5000L);
+		gpio.shutdown(); 
+		gpio.unprovisionPin(gpioPinDigitalOutput);
 		return "Light is blinking...";
 	}
 
 	@RequestMapping("/{colour}/pulse")
 	public String pulse(@PathVariable("colour") String colour) {
-		getPin(colour).pulse(5000L);
+		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), "MyLED", PinState.LOW);
+		gpioPinDigitalOutput.pulse(5000L);
+		gpio.shutdown(); 
+		gpio.unprovisionPin(gpioPinDigitalOutput);
 		return "Light is pulsing...";
-	}
-
-	private String checkState(String colour) {
-		return (getPin(colour).isHigh() ? "Light is ON" : "Light is OFF");
-	}
-
-	private GpioPinDigitalOutput getPin(String colour) {
-		return gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), "MyLED", PinState.LOW);
 	}
 }
