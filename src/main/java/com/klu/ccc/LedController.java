aeeb6df131093a3a0b9dd5c5e07789e782ac43ce
ohelig <amor.hsinay@gmail.com>
2018-10-02 03:18:58 +0530

    BugFix for GPIO pin already exists 2

--- src/main/java/com/klu/ccc/LedController.java
+++ src/main/java/com/klu/ccc/LedController.java
@@ -3,7 +3,6 @@ package com.klu.ccc;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.klu.utility.CommonUtils;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
@@ -13,7 +12,7 @@ import com.pi4j.io.gpio.PinState;
 @RestController
 public class LedController {
 	private static final GpioController gpio = GpioFactory.getInstance();
-
+	GpioPinDigitalOutput gpioPinDigitalOutput;
 	@RequestMapping("/")
 	public String greeting() {
 		return "Hello World!!!";
@@ -21,55 +20,54 @@ public class LedController {
 
 	@RequestMapping("/{colour}/toggle")
 	public String light(@PathVariable("colour") String colour) {
-		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour , PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour , PinState.LOW);
 		gpioPinDigitalOutput.toggle();
-		gpio.shutdown(); 
-		gpio.unprovisionPin(gpioPinDigitalOutput);
+		cleanUpTask();
 		return "Toggle Successful";
 	}
 
 	@RequestMapping("/{colour}/status")
 	private String getPinStatus(@PathVariable("colour") String colour) {
-		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		String status = gpioPinDigitalOutput.isHigh() ? "Light is ON" : "Light is OFF";
-		gpio.shutdown(); 
-		gpio.unprovisionPin(gpioPinDigitalOutput);
+		cleanUpTask();
 		return status;
 	}
 
 	@RequestMapping("/{colour}/on")
 	public String on(@PathVariable("colour") String colour) {
-		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		gpioPinDigitalOutput.high();
-		gpio.shutdown(); 
-		gpio.unprovisionPin(gpioPinDigitalOutput);
+		cleanUpTask();
 		return "Light is ON!!!";
 	}
 
 	@RequestMapping("/{colour}/off")
 	public String off(@PathVariable("colour") String colour) {
-		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		gpioPinDigitalOutput.low();
-		gpio.shutdown(); 
-		gpio.unprovisionPin(gpioPinDigitalOutput);
+		cleanUpTask();
 		return "Light is OFF!!!";
 	}
 
 	@RequestMapping("/{colour}/blink")
 	public String blink(@PathVariable("colour") String colour) {
-		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		gpioPinDigitalOutput.blink(200L, 5000L);
-		gpio.shutdown(); 
-		gpio.unprovisionPin(gpioPinDigitalOutput);
+		cleanUpTask();
 		return "Light is blinking...";
 	}
 
 	@RequestMapping("/{colour}/pulse")
 	public String pulse(@PathVariable("colour") String colour) {
-		GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		gpioPinDigitalOutput.pulse(5000L);
-		gpio.shutdown(); 
-		gpio.unprovisionPin(gpioPinDigitalOutput);
+		cleanUpTask();
 		return "Light is pulsing...";
+	}
+	public void cleanUpTask() {
+		this.gpioPinDigitalOutput.removeAllListeners();
+		gpio.shutdown(); 
+		gpio.unprovisionPin(this.gpioPinDigitalOutput);
 	}
 }
