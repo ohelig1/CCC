3a9d70c68b8931ab21e37867094c5587f11cb47c
ohelig <amor.hsinay@gmail.com>
2018-10-02 14:31:54 +0530

    Trying Blinking1

--- src/main/java/com/klu/ccc/LedController.java
+++ src/main/java/com/klu/ccc/LedController.java
@@ -3,17 +3,20 @@ package com.klu.ccc;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.klu.utility.CommonUtils;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPin;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
 @RestController
 public class LedController {
 	private static final GpioController gpio = GpioFactory.getInstance();
-//	GpioPinDigitalOutput gpioPinDigitalOutput;
+	private GpioPinDigitalOutput gpioPinDigitalOutput;
 	@RequestMapping("/")
 	public String greeting() {
 		return "Hello World!!!";
@@ -21,33 +24,37 @@ public class LedController {
 
 	@RequestMapping("/{colour}/toggle")
 	public String light(@PathVariable("colour") String colour) {
-		final GpioPinDigitalOutput gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour , PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour , PinState.LOW);
 		gpioPinDigitalOutput.toggle();
-		cleanUpTask(gpioPinDigitalOutput);
+	//	cleanUpTask(gpioPinDigitalOutput);
+		gpio.unprovisionPin(gpioPinDigitalOutput);
 		return "Toggle Successful";
 	}
 
 	@RequestMapping("/{colour}/status")
 	private String getPinStatus(@PathVariable("colour") String colour) {
-		final GpioPinDigitalOutput  gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		String status = gpioPinDigitalOutput.isHigh() ? "Light is ON" : "Light is OFF";
-		cleanUpTask(gpioPinDigitalOutput);
+	//	cleanUpTask(gpioPinDigitalOutput);
+		gpio.unprovisionPin(gpioPinDigitalOutput);
 		return status;
 	}
 
 	@RequestMapping("/{colour}/on")
 	public String on(@PathVariable("colour") String colour) {
-		final GpioPinDigitalOutput  gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		gpioPinDigitalOutput.high();
-		cleanUpTask(gpioPinDigitalOutput);
+		gpio.unprovisionPin(gpioPinDigitalOutput);
+	//	cleanUpTask(gpioPinDigitalOutput);
 		return "Light is ON!!!";
 	}
 
 	@RequestMapping("/{colour}/off")
 	public String off(@PathVariable("colour") String colour) {
-		final GpioPinDigitalOutput  gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		gpioPinDigitalOutput.low();
-		cleanUpTask(gpioPinDigitalOutput);
+		gpio.unprovisionPin(gpioPinDigitalOutput);
+	//	cleanUpTask(gpioPinDigitalOutput);
 		return "Light is OFF!!!";
 	}
 
@@ -55,19 +62,20 @@ public class LedController {
 	public String blink(@PathVariable("colour") String colour) {
 		final GpioPinDigitalOutput ledPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);
 		ledPin.blink(200L, 5000L);
-		cleanUpTask(ledPin);
+		gpio.unprovisionPin(gpioPinDigitalOutput);
+	//	cleanUpTask(ledPin);
 		return "Light is blinking...";
 	}
 
 	@RequestMapping("/{colour}/pulse")
 	public String pulse(@PathVariable("colour") String colour) {
-		final GpioPinDigitalOutput  gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
+		gpioPinDigitalOutput =  gpio.provisionDigitalOutputPin(CommonUtils.getPinNumber(colour), colour, PinState.LOW);
 		gpioPinDigitalOutput.pulse(5000L);
-		cleanUpTask(gpioPinDigitalOutput);
+		gpio.unprovisionPin(gpioPinDigitalOutput);	
 		return "Light is pulsing...";
 	}
 	
-	public void cleanUpTask(GpioPinDigitalOutput  gpioPinDigitalOutput) {
+	/*public void cleanUpTask(GpioPinDigitalOutput  gpioPinDigitalOutput) {
 		try {
 			gpioPinDigitalOutput.clearProperties();
 		}catch(Exception e) {
@@ -83,5 +91,5 @@ public class LedController {
 		}catch(Exception e) {
 			System.out.println("GPIO Shutdown:"+e.getMessage());
 		}		
-	}
+	}*/
 }
